0cf3266997f6af02fab7434bf9c7271ca8c12b31
521xuxiao <2501054088@qq.com>
2021-09-14 13:28:44 +0800

    webpack

--- day30webpack_optimization详解/webpack.config.js
+++ day30webpack_optimization详解/webpack.config.js
@@ -4,6 +4,8 @@
  *
  */
 
+const TerserWebpackPlugin = require("terser-webpack-plugin")
+
 
 
 module.exports = {
@@ -48,6 +50,9 @@ module.exports = {
         splitChunks: {
             chunks: 'all',
 
+            /**
+             * 以下都是默认配置
+             */
             // 分割的chunk最小为30kb（大于30kb才会被分割）
             minSize: 30 * 1024,
 
@@ -57,7 +62,49 @@ module.exports = {
             // 要提取的chunks最少被引用一次
             minChunks: 1,
 
-            
-        }
+            // 按需加载时并行加载的文件的最大的数量
+            maxAsyncRequests: 5,
+
+            // 入口js文件最大并行请求数量
+            maxInitialRequests: 3,
+
+            // 名称连接符
+            automaticNameDelimiter: '~',
+
+            // 可以使用命名规则
+            name: true,
+
+            // 分割chunk的组
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+
+                }
+            }
+        },
+        // 将当前模块的记录其它模块的hash单独打包为一个文件 runtime
+        // 解决a文件导致b文件的contenthash变化
+        runtimeChunk: {
+            name: entrypoint => `runtime-${entrypoint.name}`
+        },
+        minimizer: [
+            // 配置生产环境的压缩方案  js和css
+            new TerserWebpackPlugin({
+                // 开启缓存
+                cache: true,
+
+                // 开启多线程打包
+                parallel: true,
+
+                // 开启sourceMap
+                sourceMap: true
+            })
+        ]
     }
 }
